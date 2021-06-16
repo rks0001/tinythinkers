6cbd2a162a62fe84face2c011fd454967bfa3a58
Ranjit Kalita <ranjitkalita499@gmail.com>
2021-06-16 23:46:55 +0530

    mobile changes

--- src/sections/Section3.js
+++ src/sections/Section3.js
@@ -9,8 +9,8 @@ const Section3 = () => {
         <section>
             <Container className="textsthree">
             <h1 className="exptwo"> These are my Projects</h1>
-            <p className="desp">These are some of the Fictional projects that I worked on. I have created <br/>
-            some projects that help me understand more of the technology that I use.</p>
+            <p className="desp">These are some of the projects that I worked on. <br/>
+            I have created some projects that help me understand more of the technology that I use.</p>
             </Container>
             <Container className='project'>
             <Row>
