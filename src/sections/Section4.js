211ce4fed27b9a1c22383330205ea4484698c01f
Ranjit Kalita <ranjitkalita499@gmail.com>
2021-06-17 00:06:12 +0530

    mobile changes

--- src/sections/Section4.js
+++ src/sections/Section4.js
@@ -8,9 +8,9 @@ const Section4 = () => {
             
             <Container className="aboutme textsfour">
                 
-                <h1 className="exptwo"> About Me</h1>
+                <h1 className="exp_four"> About Me</h1>
                 <p className="descrp">Hi, I am Ranjit Kalita I am a CSE Undergrad from Tezpur University.<br/>
-                                      I am a Self-taught Front end Developer<br/>
+                                      I am a Self-taught Front end Developer.<br/>
                                     If you want to get in touch with me, send me a <a className="clr" href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=ranjitkalita5o5@gmail.com" target="_blank" rel="noreferrer">mail</a> and I will get back with you soon.</p>
                 
             </Container>
