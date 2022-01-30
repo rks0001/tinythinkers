1a950895f317fc4f116db456e043a6fd1539af84
Ranjit Kalita <ranjitkalita499@gmail.com>
2022-01-30 23:18:18 +0530

    certifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Header from './components/Header/Header'
-import { BrowserRouter, Switch as Routes, Route } from "react-router-dom";
+
 import Footer from './components/Footer/Footer'
 import Section1 from './sections/Section1'
 import Section2 from './sections/Section2'
 import Section3 from './sections/Section3'
 import Section4 from './sections/Section4'
 import Contact2 from "./components/Contact/Contact2"
-import Certifcations from './sections/Certifications/Certifications'
+
 
 
 
@@ -15,13 +15,7 @@ function App() {
   return (
 
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/certifications" element={<Certifcations />}>
-      
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  
       
       <main>
             
