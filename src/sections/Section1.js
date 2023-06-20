db78726c0f5b5864ab335a9e15765cb7589b02b1
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-06-20 20:35:14 +0530

    changes

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Container, Row, Col,Button} from 'react-bootstrap'
-import {HiDocumentDownload} from 'react-icons/hi'
+
 import './sectionone.css'
 import {Link} from 'react-scroll'
 
